731e6291a27c13da3fb3c818de8ba2c1228f161e
Amit gupta <ag09754945531@gmail.com>
2022-09-19 19:00:37 +0530

    Code updated

--- sales.rb
+++ sales.rb
@@ -1,25 +1,55 @@
-exempt = []
-import = []
-normal = []
-p "Please enter the items"
-while (input = gets.chomp) != 'exit'
-    p "Please enter the items"
-    if input.include?("book") || input.include?("pills") || input.include?("medicine") ||   input.include?("food") || input.include?("chocolate")
-        exempt << input
-    elsif input.include?("imported")
-        import << input
-    else
-        normal << input
+class Sales
+
+    @@sales_tax = 0
+    @@total = 0
+    @@exempt = []
+    @@import = []
+    @@normal = []
+    @@onlyImport = []
+    def input
+        p "Please enter the items"
+        while (input = gets.chomp) != 'exit'
+            p "Please enter the items"
+            if input.include?("imported") && input.include?("chocolates")
+                @@onlyImport << input
+            elsif input.include?("imported")
+                @@import << input
+            elsif input.include?("book") || input.include?("pills") || input.include?(  "medicine") ||    input.include?("food") || input.include?("chocolate")
+                @@exempt << input
+            else
+                @@normal << input
+            end
+        end
     end
 
-end
+    #For displayig the acutual final value of each product 
+    def finalValue 
+        @@exempt.each {|i| puts "#{i.slice(0...-8)}: #{((i.scan(/(\d+[.,]\d+)/).first.first).to_f).round(2)}"}
+        @@normal.each {|i| puts "#{i.slice(0...-8)}: #{((i.scan(/(\d+[.,]\d+)/).first.first).to_f + (i.scan(/(\d+[.,]\d+)/).first.first).to_f*(0.10)).round(2)}"}
+        @@import.each {|i| puts "#{i.slice(0...-8)}: #{((i.scan(/(\d+[.,]\d+)/).first.first).to_f + (i.scan(/(\d+[.,]\d+)/).first.first).to_f*(0.15)).round(2)}"}
+        @@onlyImport.each {|i| puts "#{i.slice(0...-8)}: #{((i.scan(/(\d+[.,]\d+)/).first.first).to_f + (i.scan(/(\d+[.,]\d+)/).first.first).to_f*(0.05)).round(2)}"}
+    end
 
-# p exempt
-# p import
-# p normal
-sales_tax = 0
-# exempt.each {|i| sales_tax += (i.scan(/(\d+[.,]\d+)/).first.first).to_f }
-normal.each {|i| sales_tax += ((i.scan(/(\d+[.,]\d+)/).first.first).to_f)* 10/100 }
-import.each {|i| sales_tax += ((i.scan(/(\d+[.,]\d+)/).first.first).to_f)* 15/100 }
+    # for sales_tax
+    def salesTax 
+        @@normal.each {|i| @@sales_tax += ((i.scan(/(\d+[.,]\d+)/).first.first).to_f)* 0.1 }
+        @@import.each {|i| @@sales_tax += ((i.scan(/(\d+[.,]\d+)/).first.first).to_f)* 0.15 }
+        @@onlyImport.each {|i| @@sales_tax += ((i.scan(/(\d+[.,]\d+)/).first.first).to_f)* 0.05 }
+        puts "Sales Taxes: #{(@@sales_tax).round(2)}" 
+    end
 
-p sales_tax
+    # For total value
+    def totalValue 
+        @@exempt.each {|i| @@total += (i.scan(/(\d+[.,]\d+)/).first.first).to_f }
+        @@normal.each {|i| @@total += ((i.scan(/(\d+[.,]\d+)/).first.first).to_f)}
+        @@import.each {|i| @@total += ((i.scan(/(\d+[.,]\d+)/).first.first).to_f)}
+        @@onlyImport.each {|i| @@total += ((i.scan(/(\d+[.,]\d+)/).first.first).to_f)}
+        puts "Total: #{(@@total + @@sales_tax).round(2)}"
+    end
+end       
+
+val = Sales.new
+val.input
+val.totalValue
+val.salesTax
+val.finalValue
